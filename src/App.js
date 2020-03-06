288a3d8cf0d985c62c684f5d3278dbdad8d97ae7
Mellieejj <Melanieverheul1992@gmail.com>
2020-03-06 11:59:23 +0100

    created form, onsubmit method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import superagent from "superagent";
 
 class App extends Component {
+  onSubmit = async event => {
+    event.preventDefault();
+    try {
+      const response = await superagent
+        .post("http://localhost:4000/message")
+        .send({ text: "hardcoded" });
+
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   render() {
-    return <div>Hello</div>;
+    return (
+      <main>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" />
+          <button>Send</button>
+        </form>
+      </main>
+    );
   }
 }
 
